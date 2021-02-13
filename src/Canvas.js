c145605258b1a477b2df157c81df5460a531f0be
fanlixingit <78702670+fanlixingit@users.noreply.github.com>
2021-02-13 14:20:57 -0500

    Change clear button name

--- src/Canvas.js
+++ src/Canvas.js
@@ -122,7 +122,7 @@ class Demo extends Component {
   render() {
     return (
       <div>
-        <button name='clearbutton' onClick={this.clear}>Aidan Fan Clear</button>
+        <button name='clearbutton' onClick={this.clear}>Clear Drawing</button>
         <CanvasDraw
           {...this.state}
           ref={canvas => this.canvas = canvas}
